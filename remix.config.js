5bf65bbd498375873cc0cc2ccb2a637f07698666
Dhanjyoti Rabha <dhanjyotir20@gmail.com>
2023-06-17 16:21:49 +0530

    wip

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,20 @@
 module.exports = {
     appDirectory: "app",
-    future: {
-        v2_routeConvention: true,
-    },
     ignoredRouteFiles: ["**/.*"],
+    server:
+      process.env.NETLIFY || process.env.NETLIFY_LOCAL
+        ? "./server.ts"
+        : undefined,
+    serverBuildPath: ".netlify/functions-internal/server.js",
+    // appDirectory: "app",
+    // assetsBuildDirectory: "public/build",
+    // publicPath: "/build/",
+    future: {
+      v2_errorBoundary: true,
+      v2_headers: true,
+      v2_meta: true,
+      v2_normalizeFormMethod: true,
+      v2_routeConvention: true,
+    },
     tailwind: true,
 }
